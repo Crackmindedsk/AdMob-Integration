79841f6f911ca092626e6c74ba60b81f02c796cd
Crackmindedsk <shakumbhar@gmail.com>
2021-06-03 15:57:37 +0530

    Banner Ad

--- app/src/main/java/sharlene/work/ads/MainActivity.kt
+++ app/src/main/java/sharlene/work/ads/MainActivity.kt
@@ -2,10 +2,20 @@ package sharlene.work.ads
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var madView: AdView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this){}
+
+        madView=findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        madView.loadAd(adRequest)
     }
 }
